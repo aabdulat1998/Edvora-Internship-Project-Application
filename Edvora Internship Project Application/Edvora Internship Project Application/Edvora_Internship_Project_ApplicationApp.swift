90ea1dfa1521164552833e845d7d15f02bcce2bf
aabdulat1998 <90874168+aabdulat1998@users.noreply.github.com>
2022-01-17 06:27:28 -0500

    Initial commit of project

---
+++ Edvora Internship Project Application/Edvora Internship Project Application/Edvora_Internship_Project_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_Internship_Project_ApplicationApp.swift
+//  Edvora Internship Project Application
+//
+//  Created by User on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_Internship_Project_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
